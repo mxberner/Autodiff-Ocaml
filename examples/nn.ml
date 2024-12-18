0be26cd82625f26d9b2f1cdbcc6e2186a4050fba
mxberner <mberner2@jh.edu>
2024-12-17 19:33:46 -0500

    update nn

--- examples/nn.ml
+++ examples/nn.ml
@@ -11,10 +11,14 @@ let () =
   let x = random ~seed [ 1; input_size ] in
   let y = random ~seed [ 1; output_size ] in
   let w = ref (random ~seed [ input_size; output_size ]) in
-  for _ = 1 to 100 do
+  for i = 1 to 100 do
     let y_pred = matmul x w.contents in
     let loss = sum (pow (y - y_pred) (Scalar (V.make 2.0))) in
-    Printf.printf "Loss: %f \n" loss.value;
+    if Int.(i % 10 = 0) then ( 
+      Printf.printf "[%d/100] Loss: %f\n" i loss.value;
+      print_endline ""
+    );
+
     let grad = V.gradients loss in
     w :=
       map
