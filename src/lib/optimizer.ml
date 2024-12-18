1f33e8b3cd47d535b612e3a074b8a6981648cc79
vbravilla <vbravilla@gmail.com>
2024-12-18 15:08:47 -0500

    removed incomplete files

--- src/lib/optimizer.ml
+++
@@ -1,8 +0,0 @@
-open Tensor
-
-[@@@ocaml.warning "-27"]
-
-type f = t -> t
-type optimizer = { init : t; step : t -> t; stop_condition : t -> bool }
-
-let gradient_descent f opt = failwith "unimplemented"
--- src/lib/optimizer.mli
+++
@@ -1,8 +0,0 @@
-(* Machine Learning optimizer used to actually do backpropogation. It is a good way to test the automatic differentiation library*)
-open Tensor
-
-type f = t -> t
-type optimizer = { init : t; step : t -> t; stop_condition : t -> bool }
-
-val gradient_descent : f -> optimizer -> t
-(** Performs gradient descent optimization on the function. *)
